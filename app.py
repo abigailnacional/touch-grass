aacb8d02cc3a65bff2c956a86b53c4d724e0cb4f
Abigail Nacional <aan16@duke.edu>
2022-03-06 07:43:59 -0500

    Got the Google Cloud Vision API working

--- app.py
+++ app.py
@@ -1,17 +1,69 @@
-from flask import Flask, render_template
-from .touchgrass import create_app
+from flask import Flask, render_template, flash, request, redirect, url_for
+from touchgrass import create_app
 import os
-from flask_sqlalchemy import SQLAlchemy, request
+from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy.orm
 
+UPLOAD_FOLDER = './static'
+
 app = create_app()
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 db = SQLAlchemy(app)
 sessionmaker = sqlalchemy.orm.sessionmaker(db.engine)
 
-# a simple page that says hello
+# index page
 @app.route("/")
 def index():
     return render_template("index.html")
+
+#Allows a user to upload an image and then calls run_quickstart on the image path
+@app.route('/upload_file', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file1' not in request.files:
+            return 'there is no file1 in form!'
+        file1 = request.files['file1']
+        path = os.path.join(app.config['UPLOAD_FOLDER'], file1.filename)
+        file1.save(path)
+        return run_quickstart(path)
+
+    return '''
+    <h1>Upload new File</h1>
+    <form method="post" enctype="multipart/form-data">
+      <input type="file" name="file1">
+      <input type="submit">
+    </form>
+    '''
+#Function run_quickstart is from https://github.com/googleapis/python-vision/blob/HEAD/samples/snippets/quickstart/quickstart.py
+#Prints the labels for the image
+def run_quickstart(imgPath):
+    # [START vision_quickstart]
+    import io
+
+    # Imports the Google Cloud client library
+    # [START vision_python_migration_import]
+    from google.cloud import vision
+    # [END vision_python_migration_import]
+
+    # Instantiates a client
+    # [START vision_python_migration_client]
+    client = vision.ImageAnnotatorClient()
+    # [END vision_python_migration_client]
+
+    # The name of the image file to annotate
+    file_name = os.path.abspath(imgPath)
+
+    # Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    # Performs label detection on the image file
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+
+    return render_template('labels.html', labels=labels)
 
 
 if __name__ == "__main__":
--- touchgrass/config.py
+++ touchgrass/config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('touchTHEgrass')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///login_user_information.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
